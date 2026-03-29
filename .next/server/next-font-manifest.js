40f8ce3fdf84641eea1a9657dba6963a49e4ae25
baljotchohan <Jattbad328@gmail.com>
2026-03-29 10:54:19 +0530

    new loading

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/baljotchohan/Desktop/LIROX/Lirox/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
