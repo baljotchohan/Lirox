ad88a03108ffbf9f309a0f126d2e0226c285a563
baljotchohan <Jattbad328@gmail.com>
2026-04-25 23:40:43 +0530

    feat: expose search_news and search_images in search tools module

--- lirox/tools/search/__init__.py
+++ lirox/tools/search/__init__.py
@@ -1,3 +1,4 @@
-from lirox.tools.search.duckduckgo import search_ddg
+from lirox.tools.search.duckduckgo import search as search_ddg
+from lirox.tools.search.duckduckgo import search, search_news, search_images
 
-__all__ = ["search_ddg"]
+__all__ = ["search", "search_ddg", "search_news", "search_images"]
